5b0acb3cab0f80334a2415b7e050e6574eb9cf03
Oliver Vassallo <olivervassallo1996@gmail.com>
2022-11-05 12:00:40 +0100

    Revert "Working UDP Server"
    This reverts commit ab0cd8118d74f2ba17b7223c325d484384441d27.

--- IQ_Ethernet_F4/Core/Src/main.c
+++ IQ_Ethernet_F4/Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "udpServerRAW.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -88,7 +88,7 @@ int main(void)
   MX_GPIO_Init();
   MX_LWIP_Init();
   /* USER CODE BEGIN 2 */
-  udpServer_init();
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
